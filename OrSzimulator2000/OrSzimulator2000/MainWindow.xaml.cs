5ef34ea66a50ffc179675663b28be6eee5961bc1
Imre Gazsó <gazso.imi@vipmail.hu>
2018-11-24 16:43:13 +0100

    belépés

---
+++ OrSzimulator2000/OrSzimulator2000/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Controls;
+
+
+
+namespace OrSzimulator2000
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Valto.pageSwitcher = this;
+         
+
+        }
+
+        public void Navigate(UserControl nextPage)
+        {
+            this.Content = nextPage;
+        }
+
+        private void Belepes(object sender, RoutedEventArgs e)
+        {
+
+            String FelhasznaloNev = felhasznalonev.Text;
+            String Jelszo = jelszo.Password.ToString();
+
+
+            using (AdatbazisEntities adat = new AdatbazisEntities())
+            {
+
+                var Felhasznalo =adat.Felhasznalok.FirstOrDefault(F => F.Felhasznalonev == FelhasznaloNev);
+
+
+                if (Felhasznalo != null)
+                {
+                    if (Felhasznalo.Jelszo == Jelszo)
+                    {
+                        Valto.Valt(new Jateknezet());
+                    }
+                    else
+                    {
+                        LoginHibauzenet.Text = "A megadott jelszó nem egyezik.";
+                    }
+                }
+                else
+                {
+                    LoginHibauzenet.Text = "Nincs Ilyen felhasználó";
+                }
+
+
+
+
+
+
+            }
+
+        }
+
+
+
+        }
+}
---
+++ OrSzimulator2000/OrSzimulator2000/Valto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+
+namespace OrSzimulator2000
+{
+    public static class Valto
+    {
+        public static MainWindow pageSwitcher;
+
+        public static void Valt(UserControl newPage)
+        {
+            pageSwitcher.Navigate(newPage);
+        }
+    }
+}
